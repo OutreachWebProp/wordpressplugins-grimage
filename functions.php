7106e96b3068f62b3dec9bd8a8e8397d3f71afe7
= <=>
2016-06-06 10:19:27 -0700

    HTML character's fixed in last commit

--- functions.php
+++ functions.php
@@ -244,7 +244,7 @@ class Grimage
 
     $dom->encoding = 'utf-8';
     $html = $dom->saveHTML();
-    return html_entity_decode($html);
+    return $html;
   }
 
   /**
@@ -270,29 +270,6 @@ class Grimage
     <!-- /Modal -->
   <?php }
 
-
-
-  public function mb_str_replace($search, $replace, $subject, &$count = 0) {
-    if (!is_array($subject)) {
-      // Normalize $search and $replace so they are both arrays of the same length
-      $searches = is_array($search) ? array_values($search) : array($search);
-      $replacements = is_array($replace) ? array_values($replace) : array($replace);
-      $replacements = array_pad($replacements, count($searches), '');
-
-      foreach ($searches as $key => $search) {
-        $parts = mb_split(preg_quote($search), $subject);
-        $count += count($parts) - 1;
-        $subject = implode($replacements[$key], $parts);
-      }
-    } else {
-      // Call mb_str_replace for each subject in array, recursively
-      foreach ($subject as $key => $value) {
-        $subject[$key] = mb_str_replace($search, $replace, $value, $count);
-      }
-    }
-
-    return $subject;
-  }
 
 }
 
