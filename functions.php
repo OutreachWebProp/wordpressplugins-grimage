b61fec08cb3ac24f18b91b1a15dd13d9587c3315
= <=>
2016-06-06 10:08:25 -0700

    testing find replace function method

--- functions.php
+++ functions.php
@@ -243,7 +243,9 @@ class Grimage
     //$dom->substituteEntities = false;
     $dom->encoding = 'utf-8';
     $html = $dom->saveHTML();
-    return $html;
+    $order   = array("Ã¡","Ã©","Ã­","Ã³","Ãº","Ã±");
+    $replace = array("&aacute","&eacute","&iacute","&oacute","&uacute","&ntilde");
+    return $this->mb_str_replace($order, $replace, $html, $count = 0);
   }
 
   /**
@@ -268,6 +270,30 @@ class Grimage
     </div>
     <!-- /Modal -->
   <?php }
+
+
+
+  public function mb_str_replace($search, $replace, $subject, &$count = 0) {
+    if (!is_array($subject)) {
+      // Normalize $search and $replace so they are both arrays of the same length
+      $searches = is_array($search) ? array_values($search) : array($search);
+      $replacements = is_array($replace) ? array_values($replace) : array($replace);
+      $replacements = array_pad($replacements, count($searches), '');
+
+      foreach ($searches as $key => $search) {
+        $parts = mb_split(preg_quote($search), $subject);
+        $count += count($parts) - 1;
+        $subject = implode($replacements[$key], $parts);
+      }
+    } else {
+      // Call mb_str_replace for each subject in array, recursively
+      foreach ($subject as $key => $value) {
+        $subject[$key] = mb_str_replace($search, $replace, $value, $count);
+      }
+    }
+
+    return $subject;
+  }
 
 }
 
